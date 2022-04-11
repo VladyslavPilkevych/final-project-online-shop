2cbdec266c62d5cd88b1094f3fa8e628557b0607
VladPilkevich <90135414+VladPilkevich@users.noreply.github.com>
2022-04-11 20:46:54 +0200

    react app skelet

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -4,7 +4,6 @@ import HomePage from '../pages/HomePage/HomePage';
 import AuthorizationPage from '../pages/AuthorizationPage/AuthorizationPage';
 import CartPage from '../pages/CartPage/CartPage';
 import FavouritePage from '../pages/FavouritePage/FavouritePage';
-import OneCardPage from '../pages/OneCardPage/OneCardPage';
 
 function AppRoutes() {
   return (
@@ -13,7 +12,6 @@ function AppRoutes() {
       <Route path="/sign-in" element={<AuthorizationPage />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/favourite" element={<FavouritePage />} />
-      <Route path="/card:id" element={<OneCardPage />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+function ProductCard() {
+  return (
+    <section>
+      <h1>Product Card</h1>
+    </section>
+  );
+}
+
+export default memo(ProductCard);
