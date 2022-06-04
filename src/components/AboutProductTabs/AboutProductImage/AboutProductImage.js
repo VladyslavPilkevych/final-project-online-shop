a838bd13eb2c46d4bc29b62d3bf427b673544820
deep-logic2000 <deeplogic2000@gmail.com>
2022-06-04 19:37:12 +0300

    Added styles & fix slide dots

--- src/components/AboutProductTabs/AboutProductImage/AboutProductImage.js
+++ src/components/AboutProductTabs/AboutProductImage/AboutProductImage.js
@@ -4,36 +4,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
 import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
 import { addToCart } from '../../../store/actionCreators/cartAC';
 import addToFavIcon from '../../../assets/Images/addToFavIcon.png';
 import styles from './AboutProductImage.module.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
+const StyledSlider = styled(Slider)`
+  &.slick-thumb {
+    display: flex !important;
+    justify-content: space-between !important;
+  }
+`;
 function AboutProductImage(props) {
   const { urls, model, id } = props;
+  console.log(urls[0]?.substring(urls[0].length - 4, urls[0].length));
+  const url = urls[0]?.substring(urls[0].length - 4, urls[0].length) === 'jpeg'
+    ? urls[0]?.substring(0, urls[0].length - 6)
+    : urls[0]?.substring(0, urls[0].length - 5);
   const dispatch = useDispatch();
   const settings = {
+    // eslint-disable-next-line react/no-unstable-nested-components
+    customPaging(i) {
+      return (
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid
+        <a className={styles.imgminWrapper}>
+          <img
+            src={urls[0]?.substring(urls[0].length - 4, urls[0].length) === 'jpeg' ? `${url}${i + 1}.jpeg` : `${url}${i + 1}.jpg`}
+            alt="model"
+            className={styles.imgmin}
+          />
+        </a>
+      );
+    },
     dots: true,
+    dotsClass: 'slick-dots slick-thumb',
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
-  console.log(urls);
   return (
-    <div className={styles.detailsWrapper}>
-      <div role="button" onClick={() => { console.log('Add to Wishlist', id); }} className={styles.addToCartIconWrapper}>
-        <img
-          src={addToFavIcon}
-          alt="Add to Cart Icon"
-          className={styles.addToCartIcon}
-        />
-      </div>
+    <div className={styles.imageSideWrapper}>
       <div className={styles.imageWrapper}>
-        <Slider {...settings}>
-          {urls && urls.map((elem) => <div key={model}><img src={`${elem}`} alt={model} className={styles.imageMain} /></div>)}
-        </Slider>
+        <StyledSlider {...settings}>
+          {urls
+          && urls.map((elem) => (
+            <div key={model}>
+              <img src={`${elem}`} alt={model} className={styles.imageMain} />
+            </div>
+          ))}
+        </StyledSlider>
       </div>
     </div>
   );
--- src/pages/AboutProductPage/AboutProductPage.js
+++ src/pages/AboutProductPage/AboutProductPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,8 +7,10 @@ import { useMatch, useLocation, useNavigate } from 'react-router-dom';
 import AboutProductDetailsTabText from '../../components/AboutProductTabs/AboutProductDetailsTabText/AboutProductDetailsTabText';
 import AboutProductCommonTabText from '../../components/AboutProductTabs/AboutProductCommonTabText/AboutProductCommonTabText';
 import AboutProductImage from '../../components/AboutProductTabs/AboutProductImage/AboutProductImage';
+import addToFavIcon from '../../assets/Images/addToFavIcon.png';
 import Button from '../../components/Button/Button';
 import { getProduct } from '../../store/actionCreators/productsAC';
+import Advantages from '../../components/Advantages/Advantages';
 
 import styles from './AboutProductPage.module.scss';
 
@@ -52,18 +55,10 @@ function AboutProductPage() {
       <hr className={styles.line} />
       <div className={`${styles.aboutProductTabMenuWrapper} ${styles.aboutProductWrapper}`}>
         <ul className={styles.aboutProductTabMenu}>
-          <li
-            className={activeTab === 'tab1' ? `${styles.active}` : ''}
-            role="menuitem"
-            onClick={() => handleToggleTab('tab1')}
-          >
+          <li className={activeTab === 'tab1' ? `${styles.active}` : ''} role="menuitem" onClick={() => handleToggleTab('tab1')}>
             About Product
           </li>
-          <li
-            className={activeTab === 'tab2' ? `${styles.active}` : ''}
-            role="menuitem"
-            onClick={() => handleToggleTab('tab2')}
-          >
+          <li className={activeTab === 'tab2' ? `${styles.active}` : ''} role="menuitem" onClick={() => handleToggleTab('tab2')}>
             Details
           </li>
         </ul>
@@ -72,28 +67,36 @@ function AboutProductPage() {
             <p className={styles.priceTitle}>Price:</p>
             <p className={styles.priceAmount}>
               {product.currency}
-              {' '}
               {product.currentPrice}
             </p>
           </div>
-          <button type="button" className={styles.aboutProductCartBtn} onClick={() => console.log('Add to Cart', product._id)}>Add to Cart</button>
+          <button type="button" className={styles.aboutProductCartBtn} onClick={() => console.log('Add to Cart', product._id)}>
+            Add to Cart
+          </button>
         </div>
       </div>
       <hr className={styles.line} />
       <div className={`${styles.aboutProducTextAndImageWrapper} ${styles.aboutProductWrapper}`}>
         <div className={styles.aboutProductTextWrapper}>
-          {activeTab === 'tab1' ? (
-            <AboutProductCommonTabText product={product} />
-          ) : (
-            <AboutProductDetailsTabText product={product} />
-          )}
+          {activeTab === 'tab1' ? <AboutProductCommonTabText product={product} /> : <AboutProductDetailsTabText product={product} />}
+        </div>
+        <div
+          onClick={() => {
+            console.log('Add to Wishlist', product._id);
+          }}
+          role="button"
+          className={styles.addToCartIconWrapper}
+        >
+          <img src={addToFavIcon} alt="Add to Cart Icon" className={styles.addToCartIcon} />
         </div>
         <div className={styles.aboutProducImageWrapper}>
           <AboutProductImage urls={product.imageUrls} model={product.model} id={product._id} />
         </div>
       </div>
+      <div>
+        <Advantages />
+      </div>
     </>
-
   );
 }
 
