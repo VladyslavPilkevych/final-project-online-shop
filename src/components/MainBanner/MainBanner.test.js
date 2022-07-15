d42714eaacf2ba43257c721c5493f1f530110ed0
Максим Яремченко <yarmaxon@gmail.com>
2022-07-15 13:55:39 +0300

    tests

--- src/components/MainBanner/MainBanner.test.js
+++ src/components/MainBanner/MainBanner.test.js
@@ -1,10 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../store';
 import MainBaner from './MainBaner';
+
+function Component() {
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <MainBaner />
+      </Provider>
+    </BrowserRouter>
+  );
+}
 
 describe('Snapshot main baner should render', () => {
   test('Snapshot main baner is rendered', () => {
-    const { asFragment } = render(<MainBaner />);
+    const { asFragment } = render(<Component />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
